75e6b9fc9795feb297a0d50c85fcb85f6a4d7561
LThames22 <likrein22@gmail.com>
2021-08-11 15:30:03 -0300

    creacion de proyectos

---
+++ bolillero.core/Bolillero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace bolillero.core
+{
+    public class Bolillero
+    {
+        byte cantidad {get; set;}
+        byte indiceAzar {get; set;}
+        List<byte> Afuera {get; set;}
+        List<byte> Adentro {get; set;}
+        Random r;
+        public Bolillero()
+        {
+            Afuera = new List<byte>{};
+            Adentro = new List<byte>{};
+            r = new Random();
+        }
+
+
+
+    }
+}
